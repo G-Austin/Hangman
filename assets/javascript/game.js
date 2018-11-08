de3ce1e816c0ed070f173b69a89bd3ac81d28cbf
Glo Austin <glo.austin19@gmail.com>
2018-11-07 18:05:12 -0800

    Fixed bugs in game.js, have a working app!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,130 @@
-//Create all the variables I may need for the game
+var wordList = ['juniper', 'douglas','fir', 
+ 'oak', 'maple', 'cypress',
+ 'cherry', 'pecan', 'chestnut', 
+ 'willow', 'redwood', 'pine', 'locust',
+ 'palm', 'hemlock'],
+ 
+//Random string from wordList 
+word = wordList[Math.floor(Math.random() * wordList.length)]//Breaks solution into number of letters
+lettersInword = [],	
+//Number of blanks based on word length(solution)
+underscores = 0,	
+//Holds a mix of blank and solved letters
+blanksAndSuccesses = [],	
+//Holds all the wrong guesses
+wrongLetter = [],	  
+//Game counters
+winCounter = 0,
+lossCounter = 0,
+guesses = 10;
+
+//FUNCTIONS - to call later
+
+function startGame() {
+	// Reset variables
+	guesses = 10;
+	blanksAndSuccesses = [];
+	wrongLetter = [];
+
+	//Breaking the word into letters
+	lettersInword = word.split("");
+
+	//count the number of letters in the word
+	underscores = lettersInword.length;
+
+	console.log('underscores', underscores)
+	console.log('word', word);
+
+		//Push the underscores to the DOM
+		for (var i = 0; i < underscores; i++) {
+		blanksAndSuccesses.push("_");
+		console.log('blanksAndSuccesses', blanksAndSuccesses);
+
+		}
 
 
- 	var wordArray = ['juniper', 'douglas','fir', 'oak', 'maple', 'cypress', 'cherry', 'pecan', 'chestnut', 'willow', 'redwood', 'pine', 'locust', 'palm', 'hemlock'],
-	 newGame = wordArray[Math.floor(Math.random() * wordArray.length)],
-	 underScores = [],
-	 guessesRemain = [],
-	 wrongWord = [],
-	 correctWord = [],
-	 winCounter = 0,
-	 wrongCounter = 0,  
-	  
+	//Reprints the guessesRemain to 10
+	document.getElementById("guessesRemain").innerHTML = ("Guesses Remaining: " + guesses);
+	//Prints the blanks at the beginning of each round in the HTML
+	document.getElementById("underScores").innerHTML = blanksAndSuccesses.join(" ");
+	//clears the wrong guesses from the previous round
+	document.getElementById("wrongWord").innerHTML = ("Guessed Letters: " + wrongLetter.join(" "));
+};
+//Create a function that does all the comparisons for matches.
+function checkLetters(letter) {
 
-//DOM
-    underScoresDiv = document.getElementById('underScores'),
-	wrongWordDiv = document.getElementById('wrongWord')
-//document.getElementById('underscores').innerHTML = genUnderscores();
-// start the game by pressing any key
+	var lettersInWord = false;
+	var letter = event.key;
 
-	console.log(newGame)
-
-	document.onkeyup = function(event) {
-		var userLetterChoice = event.key;
-		console.log(userLetterChoice);
-}
-//next I want to take the string from newGame, break it up into letters, that a function 
-//can take the length, and push out the correct number of underscores.
-var generateUnderscores = () => {
-	for (var i = 0; i < newGame.length; i++) {
-		underScores.push('_');
-
+	// Check if a letter exists insidethe array at all
+	for (var i = 0; i < underscores; i++) {
+		
+		if (word[i] === letter) {
+			console.log('word[i] ' + word[i])
+			lettersInWord = true;
+		}
 	}
-	return underScores;
-}
+// If the letter exists in the word, find which index
+	if (lettersInWord) {
+		
+		for (var j = 0; j < underscores; j++) {
+		//Populate the blanksAndSuccesses with every correct letter.
+			if (word[j] === letter) {
+			// This is where the specfic space in blanks is set and letter is equal to the letter when there is a match.
+			blanksAndSuccesses[j] = letter;
+			}
 
-console.log(generateUnderscores());
-
-//capture the users guess:
-
-document.addEventListener('keypress', (event) => {
-	var keyword = String.fromCharCode(event.keyCode);
-	console.log(keyword);
-//if users guess it right	
-	if(newGame.indexOf(keyword) > -1) {
-// add to correct word array
-		correctWord.push(keyword);
-		console.log(correctWord);
-		// targetDiv.innerHTML = underScores;
-// replace underscore with right letter
-		underScores[newGame.indexOf(keyword)] = keyword;
-		// document.getElementById('underScores' [0]);
-		underScoresDiv.innerHTML(underScores.join(' '));
-
-// checks to see if user word matches guesses
-		if(underScores.join('') == newGame) {
-			alert('You Win');
 		}
 		
 	}
+	
 	else {
-		wrongWord.push(keyword);
-		console.log(wrongWord)
+
+		wrongLetter.push(letter);
+		guesses--;
 	}
-});
+}
 
+//this is the code that runs after each guess is complete.
+function roundComplete() {
 
-//if the letter goes to the secondDiv, then wrongCounter
-//which shows guesses left, should decrease by one.
+//Update HTML with new number incorrect guesses, and correct guesses.
+document.getElementById("guessesRemain").innerHTML = ("Guesses Remaining: " + guesses);
 
-// document.userLetterChoice = function(event2) {
-// 	var userGuess = event2.keytoLowerCase();
-// 	if event2 === 
-// }
-// -Show number of wins. When you win, change the number of wins by one.
+document.getElementById("underScores").innerHTML = blanksAndSuccesses.join(" ");
 
+document.getElementById("wrongWord").innerHTML = ("Guessed Letters: " + wrongLetter.join(" "));
 
-// Show underscores based on the word length for the current word.
-	// - once the user guesses a letter in the word, replace the underscore with the letter.
-// Show the number of guesses remaining. 
-	// Each word should have 12 opportunities to guess wrong befor the user loses.
-	// if the user guess incorrectly, decrease the guesseRemain by one.
-// Show a list of the letters guessed that were incorrect.
+//If all the letters match the solution...
+if (lettersInword.toString() === blanksAndSuccesses.toString()) {
 
-//	
+	winCounter++;
+	// document.getElementById("#underscores").innerHTML = word;
+	alert("You Win! The tree is " + word);
+
+	document.getElementById("win-counter").innerHTML = ("Wins:  " + winCounter);
+	startGame();
+
+	}	
+
+else if (guesses === 0) {
+	lossCounter++;
+	alert("You lose :( the correct answer was " + word);
+
+	document.getElementById("loss-counter").innerHTML = lossCounter;
+	startGame();
+	}
+
+}
+
+startGame();
+
+document.onkeyup = function(event) {
+
+	var letterGuessed = String.fromCharCode(event.keycode).toLowerCase();
+
+	checkLetters(letterGuessed);
+
+	roundComplete();
+
+};
